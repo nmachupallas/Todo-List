16b6efdbc855c9dcf324a38453f3d734eeb1b422
Nelson Patricio Moreno Andrade <nmoreno@bce.fin.ec>
2025-07-04 10:03:05 -0500

    Se actualiza el funcionamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface Tarea {
+  id: number;
+  nombre: string;
+  completada: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-angular-1';
+  tarea = 'Todo List';
+  tareas: Tarea[] = [];
+
+  agregarTarea(nombre: string): void {
+    if (nombre.trim()) {
+      const nuevaTarea: Tarea = {
+        id: this.tareas.length + 1,
+        nombre: nombre.trim(),
+        completada: false
+      };
+      this.tareas.push(nuevaTarea);
+    }
+  }
+  eliminarTarea(id: number): void {
+    this.tareas = this.tareas.filter(tarea => tarea.id !== id);
+  }
+  
 }
